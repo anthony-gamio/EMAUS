066067b3d7ad9fcc89d9c3d64cea1d1fc58ab5d9
anthony-gamio <anthony.gamio.a@uni.pe>
2025-02-08 23:50:34 -0500

    Init SSL

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -1,19 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '323b22caac41acbf'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') 
+
+# Configuración de la base de datos
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("La variable de entorno DATABASE_URL no está configurada.")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+DATABASE_URL += "?sslmode=require"
+
+# Crear el motor y la sesión de SQLAlchemy
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Configuración para SQLAlchemy en Flask
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 db = SQLAlchemy(app)
 
 from flaskinventory import routes
 
 with app.app_context():
     db.create_all()
-    # Si tienes datos de ejemplo para insertar:
-    # nuevo_dato = ModeloEjemplo(campo='valor')
-    # db.session.add(nuevo_dato)
     db.session.commit()
